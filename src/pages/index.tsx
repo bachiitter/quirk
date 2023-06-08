6b4c9e3630582f0da24b7670ec1bd38c0ef4b1b1
Bachitter <officialbachitterchahal@gmail.com>
2023-06-08 02:35:48 -0700

    Add auth showcase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Route } from "@tanstack/router";
 
+import { useAuth, useSignOut } from "~/context/auth";
+
 import { appRoute } from "./_app";
 
 export const IndexRoute = new Route({
@@ -9,5 +11,23 @@ export const IndexRoute = new Route({
 });
 
 function Home() {
+  const { session, refreshSession } = useAuth();
+
+  const signOut = useSignOut();
+
+  if (session) {
+    return (
+      <div className="flex flex-col gap-2">
+        <p>Hello {session.user.name}</p>
+        <div className="flex gap-2">
+          <button onClick={() => refreshSession()}>
+            <span>Refresh</span>
+          </button>
+          <button onClick={() => signOut()}> SignOut</button>
+        </div>
+      </div>
+    );
+  }
+
   return <div>Hello World</div>;
 }
