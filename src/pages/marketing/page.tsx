3ea43b12aee2815ab6002196e30cb20f560d5f80
Bachitter <officialbachitterchahal@gmail.com>
2023-06-10 04:10:49 -0700

    Update home page

--- src/pages/marketing/page.tsx
+++ src/pages/marketing/page.tsx
@@ -1,7 +1,5 @@
 import { Route } from "@tanstack/router";
 
-import { useAuth, useSignOut } from "~/context/auth";
-
 import { MarketingRoute } from "./layout";
 
 export const IndexRoute = new Route({
@@ -11,23 +9,18 @@ export const IndexRoute = new Route({
 });
 
 function Home() {
-  const { session, refreshSession } = useAuth();
-
-  const signOut = useSignOut();
-
-  if (session) {
-    return (
-      <div className="flex flex-col gap-2">
-        <p>Hello {session.user.name}</p>
-        <div className="flex gap-2">
-          <button onClick={() => refreshSession()}>
-            <span>Refresh</span>
-          </button>
-          <button onClick={() => signOut()}> SignOut</button>
-        </div>
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="mx-auto flex max-w-[980px] flex-col items-center gap-2">
+        <h1 className="text-center text-4xl font-extrabold leading-tight tracking-tighter sm:text-5xl">
+          Build blazing fast web apps on the edge.
+        </h1>
+        <p className="max-w-[700px] text-center text-lg text-muted-foreground">
+          An opinionated open-source template for building full-stack web apps
+          using React and Cloudflare Pages/Functions.
+        </p>
+        <div></div>
       </div>
-    );
-  }
-
-  return <div>Hello World</div>;
+    </section>
+  );
 }
