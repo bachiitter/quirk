0229cfeb16cead968a3986c98c37be27106d5d27
Bachitter <officialbachitterchahal@gmail.com>
2023-06-12 05:48:27 -0700

    seperate get and post funcs

--- functions/api/trpc/[route].ts
+++ functions/api/trpc/[route].ts
@@ -4,7 +4,16 @@ import { appRouter } from "~/server/api/_app";
 import { createContext } from "~/server/api/trpc";
 import type { Env } from "~/server/env";
 
-export const onRequest: PagesFunction<Env> = async ({ request, env }) => {
+export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req: request,
+    router: appRouter,
+    createContext: createContext({ env, req: request }),
+  });
+};
+
+export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req: request,
