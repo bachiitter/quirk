bcd5d5ba556f1a9b60ee64a1338d7d4950b04751
Bachitter <officialbachitterchahal@gmail.com>
2023-06-12 06:05:21 -0700

    Update theme toggle width

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { Button } from "~/components/ui/button";
-import { Icons } from "~/components/ui/icons";
 import { useTheme } from "~/context/theme";
+
+import { Icons } from "./ui/icons";
 
 export function ThemeToggle() {
   const { toggleTheme } = useTheme();
 
   return (
-    <Button variant="ghost" onClick={() => toggleTheme()}>
-      <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    <Button variant="ghost" className="w-10" onClick={() => toggleTheme()}>
+      <Icons.sun className="absolute rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
