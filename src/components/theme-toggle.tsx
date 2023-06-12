cd9e9d7baafb61fef3e5f5ffe3bd1bae5241e0a4
Bachitter <officialbachitterchahal@gmail.com>
2023-06-12 06:06:17 -0700

    Change icon size

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -10,8 +10,8 @@ export function ThemeToggle() {
 
   return (
     <Button variant="ghost" className="w-10" onClick={() => toggleTheme()}>
-      <Icons.sun className="absolute rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Icons.sun className="absolute w-[23px] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Icons.moon className="absolute w-[23px] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
