a14399402a9b68f45873a12b34aba9d18e56f44f
Bachitter <officialbachitterchahal@gmail.com>
2023-06-12 06:24:37 -0700

    Remove hello route

--- functions/api/hello.ts
+++
@@ -1,8 +0,0 @@
-export function onRequestGet() {
-  return new Response(JSON.stringify({ message: "Hello World!" }), {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    status: 200,
-  });
-}
