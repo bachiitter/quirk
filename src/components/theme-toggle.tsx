41386575e9db132dec784ceb3cee0b8ebbc58cc3
Bachitter <officialbachitterchahal@gmail.com>
2023-06-16 03:19:06 -0700

    change border radius

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -9,7 +9,10 @@ export function ThemeToggle() {
   const { toggleTheme } = useTheme();
 
   return (
-    <Button variant="ghost" className="w-10" onClick={() => toggleTheme()}>
+    <Button
+      variant="ghost"
+      className="w-10 rounded-xl"
+      onClick={() => toggleTheme()}>
       <Icons.sun className="absolute w-[23px] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Icons.moon className="absolute w-[23px] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
